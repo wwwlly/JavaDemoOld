56da4bb9500ae4839be47be5d916ae42e13cb061
wangkp <969298114@qq.com>
2018-07-25 10:36:57 +0800

    Test Extends

---
+++ src/com/kemp/testjava/TestExtends.java
@@ -0,0 +1,36 @@
+package com.kemp.testjava;
+
+public class TestExtends {
+
+    private static void test(){
+        C e = new E();
+        if(e instanceof D){
+            System.out.println("instanceof");
+        }
+        C f = new F();
+        if(f instanceof D){
+            System.out.println("instanceof");
+        }
+    }
+
+    public static void main(String[] args){
+        test();
+    }
+}
+
+interface C{
+
+}
+
+interface D extends C{
+
+}
+
+class E implements D{
+
+}
+
+class F extends E{
+
+}
+
